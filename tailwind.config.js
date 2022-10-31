ac887515bf6ec7c3d9ec38ad0a26bd1b5a9a6ac7
linds0202 <lindsayarainey@gmail.com>
2022-10-31 16:26:52 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+  },
+  plugins: [],
+}
